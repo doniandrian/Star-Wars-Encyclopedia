9456c3cc2dcb3a662cbfdbeb52013a9bfcb8d29e
KevinJonathan026 <6182001026@student.unpar.ac.id>
2024-01-12 16:20:41 +0700

    tampilan page film dan template list

--- TUBES2/app/build.gradle.kts
+++ TUBES2/app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.tugas.tubes2"
+    namespace = "com.example.tesdoang"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.tugas.tubes2"
+        applicationId = "com.example.tesdoang"
         minSdk = 23
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
@@ -26,11 +26,6 @@ android {
             )
         }
     }
-
-    viewBinding{
-        enable = true
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -46,6 +41,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("com.google.android.material:material:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
